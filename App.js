4846d6f114d47a12c340359a0a06b99131d0a2e5
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2022-12-16 10:21:49 -0300

    organizing folders

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import data from "./data";
+import data from "./assets/data";
 import Card from "./Card";
 import Navbar from "./Navbar";
 
--- assets/components/Card.js
+++ assets/components/Card.js
@@ -1,5 +1,4 @@
 import React from "react";
-import data from "./data";
 
 function Card(props){
     return (
